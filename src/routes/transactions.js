775f3fff67ffaed334a04bbb3037155b939f7898
Yasas Sadeepa <yasassadeepa868@gmail.com>
2024-09-30 11:20:28 +0530

    Create transactions.js

---
+++ src/routes/transactions.js
@@ -0,0 +1,46 @@
+// models/Transaction.js
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Transaction = sequelize.define(
+    "Transaction",
+    {
+      signature: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      fromWallet: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      toWallet: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.DECIMAL(20, 8),
+        allowNull: false,
+      },
+      timestamp: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      indexes: [
+        {
+          fields: ["fromWallet"],
+        },
+        {
+          fields: ["toWallet"],
+        },
+        {
+          fields: ["timestamp"],
+        },
+      ],
+    }
+  );
+
+  return Transaction;
+};
