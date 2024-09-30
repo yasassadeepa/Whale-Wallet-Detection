70e9b31b39efd04fd066e10b1ab336a43e953c91
Yasas Sadeepa <yasassadeepa868@gmail.com>
2024-09-30 11:20:05 +0530

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,45 @@
+// src/config/database.js
+
+require("dotenv").config(); // Ensure environment variables are loaded
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false, // You can set this to false to disable logging
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // You might need to set this to true in a real production environment
+      },
+    },
+  },
+};
