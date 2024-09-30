8b98e894fa66fe15359254adc0e451b46bd96b45
Yasas Sadeepa <yasassadeepa868@gmail.com>
2024-09-30 21:01:38 +0530

    Update wallet.js

--- src/models/wallet.js
+++ src/models/wallet.js
@@ -1,41 +1,28 @@
 const { DataTypes } = require("sequelize");
-
+// models/Wallet.js
 module.exports = (sequelize) => {
-  const Wallet = sequelize.define("Wallet", {
-    address: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
+  const Wallet = sequelize.define(
+    "Wallet",
+    {
+      address: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      balance: DataTypes.DECIMAL(20, 8),
+      lastActivity: DataTypes.DATE,
+      dexActivityCount: DataTypes.INTEGER,
+      isWhale: DataTypes.BOOLEAN,
     },
-    balance: {
-      type: DataTypes.DECIMAL(20, 8),
-      allowNull: false,
-    },
-    lastActivity: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    isWhale: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    activityCount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    dailyTransactionVolume: {
-      type: DataTypes.DECIMAL(20, 8),
-      allowNull: false,
-      defaultValue: 0,
-    },
-    dailyTransactionCount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-  });
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ["address"],
+        },
+      ],
+    }
+  );
 
   return Wallet;
 };
